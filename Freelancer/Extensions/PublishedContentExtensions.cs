eced4badbb389ff5585d4b018cae98ae68612815
Paul Seal <prjseal@gmail.com>
2024-07-01 18:25:38 +0100

    all code to the end of episode 4

---
+++ Freelancer/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace Freelancer.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static HomePage? GetHomePage(this IPublishedContent publishedContent)
+        {
+            return publishedContent.AncestorOrSelf<HomePage>();
+        }
+
+        public static SiteSettings? GetSiteSettings(this IPublishedContent publishedContent) 
+        {
+            var homePage = GetHomePage(publishedContent);
+            if (homePage == null) return null;
+            return homePage.FirstChild<SiteSettings>();
+        }
+
+    }
+}
