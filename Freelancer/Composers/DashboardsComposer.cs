252e09d859293b9c182d0fb3309f6a9e402a5830
Paul Seal <prjseal@gmail.com>
2024-07-28 00:03:00 +0100

    interesting code at the end of epsiode 9

---
+++ Freelancer/Composers/DashboardsComposer.cs
@@ -0,0 +1,17 @@
+﻿using Our.Umbraco.TheDashboard.Counters.Implement;
+using Our.Umbraco.TheDashboard.Extensions;
+
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Dashboards;
+
+namespace Freelancer.Composers;
+
+public class DashboardsComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Dashboards().Remove<ContentDashboard>();
+        builder.TheDashboardCounters().Remove<MembersTotalDashboardCounter>()
+            .Remove<MembersNewLastWeekDashboardCounter>();
+    }
+}
